0ee367094439df8a0ad2252d8210ca3ea6584c09
Lethicya <lethicya.r.k@hotmail.com>
2024-09-11 12:05:52 +0900

    fundamentos 003

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-// olá mundo
-console.log('olá, mundo!')
+let meta = {
+    value: 'ler um livro por mês',
+    checked: true,
+}
+
+let metas = [
+    meta, 
+    {
+        value: 'caminhar todos os dias',
+        checked: false
+    }
+]
+
+console.log(metas[1].value)
